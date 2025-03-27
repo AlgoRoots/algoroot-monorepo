d55704f4f68a65acf1ced91f2a4cec7b35821842
AlgoRoots <algoroot524@gmail.com>
2025-03-27 18:21:51 +0900

    chore(ai): edit env

--- apps/itsme/src/modules/vector-store/lib/supabse.ts
+++ apps/itsme/src/modules/vector-store/lib/supabse.ts
@@ -4,6 +4,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const supabaseClient = createClient(
-	process.env.NEXT_PUBLIC_SUPABASE_URL!,
-	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+	process.env.SUPABASE_URL!,
+	process.env.SUPABASE_SERVICE_ROLE_KEY!,
 )
