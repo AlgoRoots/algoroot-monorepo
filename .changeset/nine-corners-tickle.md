702618e2fe001811da688c79fdfd9f3d1bc7d968
AlgoRoots <algoroot524@gmail.com>
2025-04-05 13:47:45 +0900

    fix(ai):RAG, stream 응답시간 고려 max duration 조정

---
+++ .changeset/nine-corners-tickle.md
@@ -0,0 +1,5 @@
+---
+'@algoroot/itsme': patch
+---
+
+- RAG, stream 응답시간 고려 max duration 조정
--- apps/itsme/src/app/chat/page.tsx
+++ apps/itsme/src/app/chat/page.tsx
@@ -3,7 +3,7 @@ import { HydrateClient, prefetch, trpc } from '@/modules/api/trpc/server'
 import { ChatQuestionSheet } from '@/components/Chat/@parts/ChatQuestionSheet'
 import ChatInterface from '@/components/Chat/ChatInterface'
 
-export const maxDuration = 30
+export const maxDuration = 60
 
 export default async function ChatPage() {
 	await prefetch(trpc.getSuggestQuestions.queryOptions())
